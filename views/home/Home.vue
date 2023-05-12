a42b49d1f118200f4c11c615d5a5e430ac924556
DESKTOP-NPNU3UH\杨二贺 <982289931@qq.com>
2023-05-12 09:58:04 +0800

    页面基本修改完成

--- views/home/Home.vue
+++ views/home/Home.vue
@@ -2,10 +2,27 @@
     <div style="margin-top: 60px;">
 
         <div class="table-page" >
-            <el-table :data="tableData" stripe border>
-                <el-table-column prop="name" label="姓名"></el-table-column>
-                <el-table-column prop="age" label="年龄"></el-table-column>
-                <el-table-column prop="address" label="地址"></el-table-column>
+            <div style="text-align: right; height: 50px;">
+                <el-button type="primary">添加</el-button>
+            </div>
+            
+            <el-table :data="tableData" :cell-style="{'text-align':'center'}" :header-cell-style="{'text-align':'center'}"  border>
+                <el-table-column type="index" width="50" />
+                <el-table-column prop="date" sortable  label="日期"></el-table-column>
+                <el-table-column prop="frontEndVersion" label="前端版本"></el-table-column>
+                <el-table-column prop="frontEndAddress" label="前端地址"></el-table-column>
+                <el-table-column prop="backEndersion" label="后端版本"></el-table-column>
+                <el-table-column prop="backEndAddress" label="后台地址"></el-table-column>
+                <el-table-column prop="environment" label="环境(test/prod)" width="150px"></el-table-column>
+                <el-table-column prop="deployer" label="部署人"></el-table-column>
+                <el-table-column prop="message" label="备注"></el-table-column>
+                <el-table-column fixed="right" label="操作" width="120">
+                    <template #default>
+                        <el-button link type="primary" size="small" >编辑</el-button>
+                        <el-button link type="danger" size="small">删除</el-button>
+                    </template>
+                </el-table-column>
+
             </el-table>
             <el-pagination
                 @size-change="handleSizeChange"
@@ -48,9 +65,16 @@ export default {
         getTableData() {
             // 发送请求获取表格数据，这里简化为直接赋值
             this.tableData = [
-                { name: '张三', age: 18, address: '北京市' },
-                { name: '李四', age: 20, address: '上海市' },
-                { name: '王五', age: 22, address: '广州市' },
+                { date: '2020-02-18', frontEndVersion: 18.1, backEndersion:'10.1' ,frontEndAddress: 'v7.0.1', backEndAddress: 'v5.0.1', environment:'test', deployer:'人员1', message:"22:00"  },
+                { date: '2020-01-18', frontEndVersion: 20.1, backEndersion:'10.1' ,frontEndAddress: 'v7.0.1', backEndAddress: 'v5.0.1', environment:'test', deployer:'人员1', message:"22:00"  },
+                { date: '2020-01-18', frontEndVersion: 20.1, backEndersion:'10.1' ,frontEndAddress: 'v7.0.1', backEndAddress: 'v5.0.1', environment:'test', deployer:'人员1', message:"22:00"  },
+                { date: '2020-01-18', frontEndVersion: 20.1, backEndersion:'10.1' ,frontEndAddress: 'v7.0.1', backEndAddress: 'v5.0.1', environment:'test', deployer:'人员1', message:"22:00"  },
+                { date: '2020-01-18', frontEndVersion: 20.1, backEndersion:'10.1' ,frontEndAddress: 'v7.0.1', backEndAddress: 'v5.0.1', environment:'test', deployer:'人员1', message:"22:00"  },
+                { date: '2020-01-18', frontEndVersion: 20.1, backEndersion:'10.1' ,frontEndAddress: 'v7.0.1', backEndAddress: 'v5.0.1', environment:'test', deployer:'人员1', message:"22:00"  },
+                { date: '2020-01-18', frontEndVersion: 20.1, backEndersion:'10.1' ,frontEndAddress: 'v7.0.1', backEndAddress: 'v5.0.1', environment:'test', deployer:'人员1', message:"22:00"  },
+                { date: '2020-01-18', frontEndVersion: 20.1, backEndersion:'10.1' ,frontEndAddress: 'v7.0.1', backEndAddress: 'v5.0.1', environment:'test', deployer:'人员1', message:"22:00"  },
+                { date: '2020-01-18', frontEndVersion: 20.1, backEndersion:'10.1' ,frontEndAddress: 'v7.0.1', backEndAddress: 'v5.0.1', environment:'test', deployer:'人员1', message:"22:00"  },
+                { date: '2020-02-18', frontEndVersion: 22.1, backEndersion:'10.1' ,frontEndAddress: 'v7.0.1', backEndAddress: 'v5.0.1', environment:'test', deployer:'人员1', message:"22:00"  },
                 // ...
             ];
             this.total = this.tableData.length;
